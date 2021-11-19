d2ac52e2952b4d8d844443d10118d1d50765d270
jobayercse71 <jobayercse71.com>
2021-11-19 23:45:44 +0600

    port2

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ admin.initializeApp({
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ipq6z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
