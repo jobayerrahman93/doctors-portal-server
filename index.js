050e2da3a1a0a9fac7929f9e640ae1c0ea58e17f
jobayercse71 <jobayercse71.com>
2021-11-19 23:57:41 +0600

    port change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const fileUpload=require('express-fileupload');
-const port = process.env || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
